ef289c137c07703b6c003fa6aed6a77049abbd37
M-Abdulhakim <143715405+M-Abdulhakim@users.noreply.github.com>
2023-10-05 11:33:35 +0200

    while loops

---
+++ main.js
@@ -0,0 +1,96 @@
+// Opdracht 1
+console.log("Opdracht 1");
+let counter = 0;
+while (counter < 10) {
+  console.log(counter);
+  counter++;
+}
+console.log("Einde programma");
+
+// Opdracht 2
+console.log("Opdracht 2");
+counter = 2;
+while (counter <= 20) {
+    console.log("Number is:", counter);
+    counter++;
+}
+console.log("Einde programma");
+
+
+// Opdracht 3
+console.log("Opdracht 3");
+ counter = 10;
+while (counter >= 0) {
+    console.log(counter);
+    counter--;
+}
+console.log("Einde programma");
+
+
+
+// Opdracht 4
+console.log("Opdracht 4");
+ counter = 40;
+while (counter >= 4) {
+  console.log(counter);
+  counter--;
+}
+console.log("Einde programma");
+
+
+// Opdracht 5
+console.log("Opdracht 5");
+ counter = 0;
+while (counter < 8) {
+  console.log("Ik ben een student van ROC Mondriaan");
+  counter++;
+}
+console.log("Einde programma");
+
+
+// Opdracht 6
+console.log("Opdracht 6");
+ counter = 0;
+while (counter <= 12) {
+  console.log("Dit is loop: " + counter);
+  counter++;
+}
+console.log("Einde programma");
+
+
+// Opdracht 7
+console.log("Opdracht 7");
+while (true) {
+  for (let i = 0; i < 10; i++) {
+    console.log(i);
+  }
+  break;
+}
+console.log("Einde programma");
+
+// // Opdracht 8
+console.log("Opdracht 8");
+ counter = 0;
+while (counter < 20) {
+  console.log("Loop");
+  counter++;
+  if (counter === 10) {
+    break;
+  }
+}
+console.log("Einde programma");
+
+// // Opdracht 9
+console.log("Opdracht 9");
+ counter = 0;
+while (counter < 20) {
+  if (counter < 10) {
+    console.log("Eerste zin");
+  } else {
+    console.log("Tweede zin");
+  }
+  counter++;
+}
+console.log("Einde programma");
+
+
